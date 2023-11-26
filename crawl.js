401b209c48c3fccc0d35162ba4636339dc767d98
Andrew <andrewunifei@gmail.com>
2023-11-26 13:33:16 -0300

    Crawling pronto

--- crawl.js
+++ crawl.js
@@ -1,30 +1,54 @@
 const { JSDOM } = require('jsdom')
 
-async function crawlPage(url){
-    console.log(`We're crawling ${url}`)
+async function crawlPage(baseURL, currentURL, pages){
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+
+    if(baseURLObj.hostname !== currentURLObj.hostname) return pages
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+
+    if(pages[normalizedCurrentURL] > 0){
+        pages[normalizedCurrentURL]++
+
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
+
+    console.log(`We're crawling ${currentURL}`)
 
     try{
-        const res = await fetch(url, {
+        const res = await fetch(currentURL, {
             method: 'GET',
             mode: 'cors'
         }) 
 
         if(res.status > 399){
-            console.log(`Error in fetch with status code ${res.status}, on page ${url}`)
-            return
+            console.log(`Error in fetch with status code ${res.status}, on page ${currentURL}`)
+            return pages
         }
 
         const contentType = res.headers.get('content-type')
 
         if(!contentType.includes('text/html')){
-            console.log(`Non HTML response. Content-Type is ${contentType}, on page ${url}`)
-            return
+            console.log(`Non HTML response. Content-Type is ${contentType}, on page ${currentURL}`)
+            return pages
         }
 
-        console.log(await res.text())
+        const HTMLBody = await res.text()
+
+        nextURLs = getURLsFromHTML(HTMLBody, baseURL)
+
+        for(const nextURL of nextURLs){
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
+
     } catch(err){
-        console.log(`Error in fetch: ${err.message}, on page ${url}`)
+        console.log(`Error in fetch: ${err.message}, on page ${currentURL}`)
     }
+
+    return pages
 }
 
 function getURLsFromHTML(HTMLBody, baseURL){
